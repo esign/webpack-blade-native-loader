d5781648137c14af8260c7363d0238fbfb609c2f
Bart Decorte <bart@esign.eu>
2023-08-23 11:54:30 +0200

    Add: clarifying comments

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $viewPath = __DIR__ . '/../../' . $viewDir;
 $source = file_get_contents($opts['source']);
 $cachePath = __DIR__ . '/cache';
 
+// Write template source to a random temporary file
 $currentViewFilename = '__' . md5(rand(0, 999999));
 while (file_exists($currentViewPath = "$viewPath/$currentViewFilename.blade.php")) {
     $currentViewFilename = '__' . md5(rand(0, 999999));
@@ -20,12 +21,15 @@ file_put_contents($currentViewPath, $source);
 
 require __DIR__ . '/factory.php';
 
+// Render the template contained in the temporary file created above
 try {
     $compiled = $factory->make($currentViewFilename)->render();
 } finally {
+    // Clean-up, always, even if an error occurred
     unlink($currentViewPath);
 }
 
+// Write the compilation result
 $handle = fopen($opts['out'], 'w');
 fwrite($handle, $compiled);
 fclose($handle);
